a8d48ea7c64a78e078103b36c1c85b434a421e53
Yhaunn <98993779+Yhaunn@users.noreply.github.com>
2026-02-20 04:08:33 -0500

    Module 4 assignment added

--- Module 3/ConsoleApp2/obj/Release/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ Module 3/ConsoleApp2/obj/Release/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d2bb611dc8d0fe21989e269729b79d253ca78a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
---
+++ Module 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input first number (Dividend).");
+            string inp1 = Console.ReadLine();
+
+            Console.WriteLine("Input second number (Divisor).");
+            string inp2 = Console.ReadLine();
+
+            try
+            {
+                int dividend = Convert.ToInt32(inp1);
+                int divisor = Convert.ToInt32(inp2);
+
+                int quotient = Divide(dividend, divisor);
+                int remainder = CalculateRemainder(dividend, divisor);
+
+                Console.WriteLine($"{dividend} divided by {divisor} is {quotient}, with a remainder of {remainder}");
+            }
+            catch (OverflowException ex)
+            {
+                // Console.WriteLine("Erorr: Number entered is too large to be converted into a 32-bit integer. ");
+                OutputExceptionInfo(ex);
+            }
+            catch (FormatException ex)
+            {
+                // Console.WriteLine("Error: Number entered is of an invalid format.");
+                OutputExceptionInfo(ex);
+            }
+            catch (DivideByZeroException ex)
+            {
+               //  Console.WriteLine("Error: Cannot divide by zero.");
+                OutputExceptionInfo(ex);
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("An unexpected error has occured.");
+                OutputExceptionInfo(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit program...");
+                Console.ReadKey();
+
+                Environment.Exit(0);
+            }
+        }
+
+        static int Divide(int a, int b) => a / b;
+        static int CalculateRemainder(int a, int b) => a % b;
+
+        static void OutputExceptionInfo(Exception ex)
+        {
+            Console.WriteLine($"Message: {ex.Message}");
+            Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+            Console.WriteLine($"Data: {ex.Data}");
+        }
+    }
+}
+    
